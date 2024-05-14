fc3cafd8396aaeb0df2f766c66b07cc701456065
Viezas <dxviezas@gmail.com>
2024-05-14 18:00:03 +0200

    Add cell state checking

--- main.go
+++ main.go
@@ -3,5 +3,49 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("GO Game of Life project")
+	firstGeneration := [3][3]bool{{true, false, true}, {false, true, false}, {false, false, true}}
+	var nextGeneration [3][3]bool
+
+	for rowIndex := 0; rowIndex < len(firstGeneration); rowIndex++ {
+		row := firstGeneration[rowIndex]
+
+		for colIndex := 0; colIndex < len(row); colIndex++ {
+			neighborCount := calculateNeighborCount()
+			alive := row[colIndex]
+
+			if alive && (neighborCount == 2 || neighborCount == 3) {
+				// KEEP CELL ALIVE
+				nextGeneration[rowIndex][colIndex] = true
+			} else if !alive && neighborCount == 3 {
+				// REVIVE DEAD CELL
+				nextGeneration[rowIndex][colIndex] = true
+			} else {
+				// KILL CELL because of LONELINESS or OVERPOPULATION or cell was ALREADY DEAD
+				nextGeneration[rowIndex][colIndex] = false
+			}
+		}
+	}
+	fmt.Println("First generation:")
+	printCells(firstGeneration)
+
+	fmt.Println("Next generation:")
+	printCells(nextGeneration)
+}
+
+func calculateNeighborCount() int {
+	return 5
+}
+
+func printCells(cells [3][3]bool) {
+	for rowIndex := 0; rowIndex < len(cells); rowIndex++ {
+		row := cells[rowIndex]
+		for colIndex := 0; colIndex < len(row); colIndex++ {
+			if row[colIndex] {
+				fmt.Print("O")
+			} else {
+				fmt.Print("#")
+			}
+		}
+		fmt.Println()
+	}
 }
